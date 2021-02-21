2f1d4156702bc6d1631cb60fb85634efe8a31ebc
Tanish Chouhan <chouhantanish@gmail.com>
2021-02-22 00:26:18 +0530

    Added Odometer Number Animations

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from "react";
+import { React, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./App.css";
-import Header from './components/Header';
-import Avatar from './components/Avatar';
-import AboutMe from './components/AboutMe'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Avatar from "./components/Avatar";
+import AboutMe from "./components/AboutMe";
+import Counters from "./components/Counters";
 
 function App() {
+  
+
   return (
-    <Container fluid style={{ background: 'radial-gradient(rgb(36, 35, 35), #000)' }}>
+    <Container
+      fluid
+      style={{ background: "rgb(0,0,0,0.94)" }}
+    >
       <Header />
       <Container>
         <Row className="firstSection">
@@ -20,7 +27,11 @@ function App() {
             <AboutMe />
           </Col>
         </Row>
+        <Row>
+          <Counters className="my-5"/>
+        </Row>
       </Container>
+      {/* <Footer /> */}
     </Container>
   );
 }

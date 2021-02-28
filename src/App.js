071a5f138fca92d2b3079b9321befda9b2249da4
Tanish Chouhan <chouhantanish@gmail.com>
2021-02-28 19:10:29 +0530

    Added Certification

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import AboutMe from "./components/AboutMe";
 import Counters from "./components/Counters";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-
+import Certificates from "./components/Certificates";
 function App() {
 
 
@@ -38,6 +38,12 @@ function App() {
           <h1 className="text-center display-4 text-white">Skills.</h1>
           <Row>
             <Skills />
+          </Row>
+        </Row>
+        <Row className="d-flex flex-column justify-content-center bg-danger mt-5 p-2" id="skills" style={{background: `radial-gradient(rgb(36, 35, 35), #000)`}}>
+          <h1 className="text-center display-4 text-white">Certificates.</h1>
+          <Row>
+            <Certificates />
           </Row>
         </Row>
       </Container>
---
+++ src/components/Certificates.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import CNptel from "../img/C-Nptel.jpg";
+import Algo_box from '../img/algoritmic_toolbox.png';
+import Right from '../img/rightIcon.png';
+import Left from '../img/leftIcon.png';
+
+function Certificates() {
+    return (
+        <div className="m-auto py-3">
+            <style>
+                {`
+                  .carousel-indicators li {
+                    background-color: gray;
+                  }
+                  .carousel-indicators .active {
+                    background-color: black;
+                  }
+                  .carousel-control-next-icon{
+                      background-image : url(${Right});
+                  }
+                  .carousel-control-prev-icon{
+                    background-image : url(${Left});
+                  }
+                `}
+            </style>
+            <Carousel>
+                <Carousel.Item interval={3000}>
+                    <img
+                        className="d-block"
+                        src={Algo_box}
+                        alt="First slide"
+                        style={{ height: '80vh', width: '55vw' }}
+                    />
+                </Carousel.Item>
+                <Carousel.Item interval={3000}>
+                    <img
+                        className="d-block"
+                        src={CNptel}
+                        alt="Second slide"
+                        style={{ height: '80vh', width: '55vw' }}
+                    />
+                </Carousel.Item>
+            </Carousel>
+        </div>
+
+    )
+}
+
+export default Certificates
